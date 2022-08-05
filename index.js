a20fb756f771577c0d703c6f0410f2123df1f324
IcyKitMac <icykitdesign@gmail.com>
2022-08-05 17:16:18 +0300

    Updated: Telegram API support added

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ import {
 
 import shedule from './shedule.js';
 
-// import TelegramBot from 'node-telegram-bot-api'
-// const token = '5421280288:AAFi5W4IyLEy2Ez5yaFcOqa_s-VHlfvLIjI';
-// const bot = new TelegramBot(token, {polling: true});
+import TelegramBot from 'node-telegram-bot-api'
+const token = '5421280288:AAFi5W4IyLEy2Ez5yaFcOqa_s-VHlfvLIjI';
+const bot = new TelegramBot(token, {polling: true});
 
-// bot.on('message', (msg) => {
-//     if (msg.text === 'Привет') {
-//         bot.sendMessage(msg.chat.id, 'Привет!')
-//     }
-// });
+bot.on('message', (msg) => {
+    if (msg.text === 'Привет') {
+        bot.sendMessage(msg.chat.id, 'Привет!')
+    }
+});
 
 const startDate = new Date(2022, 7, 5, 16, 49);
 
